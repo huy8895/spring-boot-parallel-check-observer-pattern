993ccbf05aa8b09525ae7fb675b2a6e4da1d0eb6
huy8895 <huytv.ous@mcredit.com.vn>
2023-02-15 09:25:08 +0700

    refactor

--- src/main/java/com/example/springbootbasecrud/repository/BaseCRUDRepository.java
+++ src/main/java/com/example/springbootbasecrud/repository/BaseCRUDRepository.java
@@ -2,11 +2,13 @@ package com.example.springbootbasecrud.repository;
 
 import com.example.springbootbasecrud.entity.BaseCRUDEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.Optional;
 
 @NoRepositoryBean
-public interface BaseCRUDRepository<E extends BaseCRUDEntity, ID> extends JpaRepository<E, ID> {
+public interface BaseCRUDRepository<E extends BaseCRUDEntity, ID> extends JpaRepository<E, ID> ,
+        JpaSpecificationExecutor<E> {
     Optional<E> findByIdAndDeletedFlagFalse(Long Id);
 }
--- src/main/java/com/example/springbootbasecrud/repository/CategoryRepository.java
+++ src/main/java/com/example/springbootbasecrud/repository/CategoryRepository.java
@@ -1,11 +1,8 @@
 package com.example.springbootbasecrud.repository;
 
 import com.example.springbootbasecrud.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends BaseCRUDRepository<Category, Long>,
-        JpaSpecificationExecutor<Category> {
+public interface CategoryRepository extends BaseCRUDRepository<Category, Long>{
 }
