2989642dc5611d878bdf55d9bde196f6d6cfc493
huytv <huy8895@gmail.com>
2023-02-16 23:05:47 +0700

    done swagger v3

--- src/main/java/com/example/springbootbasecrud/SpringBootBaseCrudApplication.java
+++ src/main/java/com/example/springbootbasecrud/SpringBootBaseCrudApplication.java
@@ -1,9 +1,12 @@
 package com.example.springbootbasecrud;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "demo swagger base crud"))
 public class SpringBootBaseCrudApplication {
 
     public static void main(String[] args) {
