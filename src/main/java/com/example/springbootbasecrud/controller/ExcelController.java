4c1bc92a2af3383c06731af8f5092e586fb03e34
huy8895 <huytv.ous@mcredit.com.vn>
2023-02-20 16:45:49 +0700

    [excel] base #3

---
+++ src/main/java/com/example/springbootbasecrud/controller/ExcelController.java
@@ -0,0 +1,31 @@
+package com.example.springbootbasecrud.controller;
+
+import com.example.springbootbasecrud.entity.Product;
+import com.example.springbootbasecrud.helper.excel.ExcelHelper;
+import com.example.springbootbasecrud.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/excel")
+@RequiredArgsConstructor
+public class ExcelController {
+    private final ExcelHelper<Product> excelHelper;
+    private final ProductService productService;
+
+
+    @GetMapping("/product")
+    public ResponseEntity<?> export(){
+        Page<Product> productPage = productService.findAll(Pageable.ofSize(10));
+        byte[] writeFile = excelHelper.writeFile(productPage.getContent());
+        return ResponseEntity.ok(writeFile)
+                ;
+    }
+}
--- src/main/java/com/example/springbootbasecrud/entity/Product.java
+++ src/main/java/com/example/springbootbasecrud/entity/Product.java
@@ -2,11 +2,15 @@ package com.example.springbootbasecrud.entity;
 
 
 import com.example.springbootbasecrud.base.BaseCRUDEntity;
+import com.example.springbootbasecrud.helper.excel.CellDTO;
+import com.example.springbootbasecrud.helper.excel.ExcelPOJO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
 import lombok.experimental.Accessors;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,10 +21,18 @@ import lombok.experimental.Accessors;
 @AllArgsConstructor
 @Entity
 @Table(name = "product")
-public class Product extends BaseCRUDEntity {
+public class Product extends BaseCRUDEntity implements ExcelPOJO {
     @Column(name = "name")
     public String name;
 
     @Column(name = "category_id")
     public Long categoryId;
+
+    @Override
+    public List<CellDTO> getCells() {
+        return List.of(CellDTO.builder()
+                              .index(0)
+                              .value(String.valueOf(id))
+                              .build());
+    }
 }
